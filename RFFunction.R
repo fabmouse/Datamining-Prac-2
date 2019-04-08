3862b84bd5e8244ac700530aa120a498567b1d95
Yaxiong <leiyx.psy@hotmail.com>
2019-04-08 18:40:23 +0100

    add some changes

--- RFFunction.R
+++ RFFunction.R
@@ -4,12 +4,16 @@ randomForestTest <-
     y.name <- unlist(strsplit(as.character(MODEL$call),split=" ")[[2]])[1]
     y.pos <- which(names(HOLDOUT)==y.name)
 
+  #  misclsssNumber <- list()
+   # misclassRate <- list()
+  #  accuracyValue <- list()
     #Random forest
         L <- levels(HOLDOUT[,2])
         predicteds <- predict(MODEL,newdata=HOLDOUT)
         actuals <- HOLDOUT[,2]
         T<-table(actuals,predicteds,dnn=c())
         accuracy <- sum(diag(T))/(sum(T))
+        total <- sum(T)
         misclass.holdout <- (T[1,2]+T[1,3]+T[1,4]+T[2,1]+T[2,3]+T[2,4]+T[3,1]+T[3,2]+T[3,4]+T[4,1]+T[4,2]+T[4,3])
         misclass.rate <- sum(misclass.holdout)/sum(T)
         
@@ -18,7 +22,9 @@ randomForestTest <-
         colnames(T) <- c(paste(L),"Total")
         rownames(T) <- c(paste(L),"Total")
         
-        return(list(CM.holdout=T, misclass.holdout=misclass.holdout, misclass.rate=misclass.rate, accuracy=accuracy)) 
+        result <- list( 'Total' = total, 'Misclass' = misclass.holdout, 'Accuracy' = accuracy,'Misclass Rate' = misclass.rate)
+        tablevalue <- data.frame(do.call(cbind,result))
+        return(list('Confusion matrix:' = T, 'Summary:' = tablevalue)) 
   }
 
 CVgroup <- function(k, dataset, seed){
