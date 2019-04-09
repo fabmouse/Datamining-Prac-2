42e8b030d6659f7abef7517dd99d125d9c2c337a
Yaxiong <leiyx.psy@hotmail.com>
2019-04-09 01:37:11 +0100

    random forest for predict remain or leave EU

--- Scripts/RF_P2.R
+++ Scripts/RF_P2.R
@@ -1,7 +1,7 @@
 ## Random Forest
-brexitData <- read.csv("data/Final Downloaded Data.csv", header = T)
+brexitData <- read.csv("data/remainORleave.csv", header = T)
 head(brexitData)
-table(brexitData$Constituency)
+table(brexitData$LeaveRemain)
 library(plyr)
 library(randomForest)
 source("RFFunction.R")
@@ -11,7 +11,7 @@ set.seed(100)
 # Trying without validation ----------------------------------------------------
 #ON ALL DATA
 #Fit the Random Forest
-original.rf = randomForest(factor(Constituency)~ Voting.1+Voting.2+Voting.3+Voting.4+Voting.5+Voting.6+Voting.7+Voting.8,data = brexitData)
+original.rf = randomForest(factor(LeaveRemain)~ Voting.1+Voting.2+Voting.3+Voting.4+Voting.5+Voting.6+Voting.7+Voting.8,data = brexitData)
 ##View the forest results
 print(original.rf)
 round(importance(original.rf),2)
