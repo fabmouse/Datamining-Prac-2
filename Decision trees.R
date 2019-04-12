a0c8f464f3782f3061a76be29a12df388a054339
jpmenb1 <josepedrobaltazar@hotmail.com>
2019-04-12 18:14:58 +0100

    dec trees

--- Decision trees.R
+++ Decision trees.R
@@ -1,24 +1,41 @@
 library(tree)
 library(ISLR)
+
+#import data, might change data set
 voteData <- read.csv("data/Clean Vote Data.csv", header=T)
 attach(voteData)
 
-
+#decision tree fit
 tree.voting <- tree(Party ~ Voting.1 + Voting.2 + Voting.3 + Voting.4 +
                       Voting.5 + Voting.6 + Voting.7 + Voting.8, data = voteData)
 
+#plot of decision tree
 plot(tree.voting)
 text(tree.voting, pretty = 0)
 
-set.seed(20)
-train <- sample(1:nrow(voteData), 450)
+#training the decision tree, using 500 rows of the data set, might change the training size
+set.seed(22)
+train <- sample(1:nrow(voteData), 500)
+voteData.test <- voteData[train,]
+Party.test <- Party[train]
+tree.voting <- tree(Party ~ Voting.1 + Voting.2 + Voting.3 + Voting.4 +
+                      Voting.5 + Voting.6 + Voting.7 + Voting.8, data = voteData, subset = train)
+tree.pred <- predict(tree.voting, voteData.test, type = "class")
+
+#classification tree. unsure about the results
+table(tree.pred, Party.test)
+(40+43+5+8)
+  
+set.seed(23)
+train <- sample(1:nrow(voteData), 100)
 voteData.test <- voteData[-train,]
 Party.test <- Party[-train]
 tree.voting <- tree(Party ~ Voting.1 + Voting.2 + Voting.3 + Voting.4 +
                       Voting.5 + Voting.6 + Voting.7 + Voting.8, data = voteData, subset = train)
 tree.pred <- predict(tree.voting, voteData.test, type = "class")
+
+#classification tree. unsure about the results
 table(tree.pred, Party.test)
-(169+153+12+25)/(169+1+16+3+1+153+1+6+0+3+12+12+0+0+0+25)#0.893
 
 set.seed(21)
 
@@ -35,4 +52,8 @@ text(prune.voting, pretty = 0)
 
 tree.pred2 <- predict(prune.voting, voteData.test, type = "class")
 table(tree.pred2, Party.test)
+
+
+tree.pred2
+with(voteData[train,], table(tree.pred2, Party.test))
 
